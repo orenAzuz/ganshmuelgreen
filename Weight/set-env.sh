62425fc4a65f7171cc96afd5b8441795f6595424
StackTracer <stacnospam@gmail.com>
2019-04-14 18:50:04 +0300

    docker compose

---
+++ Weight/set-env.sh
@@ -0,0 +1,3 @@
+# read in via: source <filename>
+export APP_DATA_PATH=/home/system/devops/w3/ganshmuelgreen/Weight/app/in
+export DB_DATA_PATH=/home/system/devops/w3/ganshmuelgreen/Weight/mysql/datadir
