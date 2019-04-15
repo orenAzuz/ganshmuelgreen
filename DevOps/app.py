f4d033567152793dfce0007d926b408e2400898a
orenAzuz <orezaz@gmail.com>
2019-04-15 16:12:53 +0300

    send mail

--- DevOps/app.py
+++ DevOps/app.py
@@ -4,8 +4,24 @@ from flask import Flask, render_template, request, json
 import subprocess
 
 #PORT = 8000
+
+from flask_mail import Mail, Message
 HOST = "0.0.0.0"
 app = Flask(__name__)
+
+
+app.config.update(dict(
+	DEBUG=True,
+	MAIL_SERVER='smtp.gmail.com',
+	MAIL_PORT=587,
+	MAIL_USE_TLS=True,
+	MAIL_USE_SSL=False,
+    MAIL_USERNAME='green.develeap@gmail.com',
+    MAIL_PASSWORD='1j4u_ore'
+))
+
+mail = Mail()
+mail.init_app(app)
 
 @app.route('/')
 def api_root():
@@ -15,7 +31,7 @@ def api_root():
 
 @app.route("/health")
 def api_health():
-	mestxt = "HEALTH OK"
+	mestxt = "HEALTH OK: new deploy from git. branch -> master. "
 	return render_template('index.html',message=mestxt)
 
 @app.route("/reload",methods=['POST'])
@@ -24,7 +40,28 @@ def api_reload():
 	print('request.query_string : ',request.query_string)
 	subprocess.call(['./reload.sh'])
 	mestxt = "RELOAD OK"
+	send_mail(mestxt)
 	return render_template('index.html',message=mestxt)
+
+
+# @app.route("/mail", methods=['GET'])
+def send_mail(message):
+
+	msg = Message(message, sender='webmykitchen@gmail.com', recipients=['orezaz@gmail.com',
+																	   'yaniv.d@develeap.com',
+																	   'nirdod@gmail.com',
+																	   'giuliovnturi@gmail.com'
+																	   'ilana.fisher.il@gmail.com',
+																	   'razleshem3@gmail.com',
+																	   'br.cohen@hotmail.fr',
+																		'emanaz.91@gmail.com',
+																		'sharontabakman@gmail.com',
+																		'stacnospam@gmail.com'])
+	msg.body = 'This is a test email'  # Customize based on user input
+	mail.send(msg)
+
+	return 'done'
+
 
 if __name__ == "__main__":
 #	app.run(port=PORT, host = HOST, debug=True)
