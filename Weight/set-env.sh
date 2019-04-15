d734655528b27abab8b600e8c7b6a53556ad4466
StackTracer <stacnospam@gmail.com>
2019-04-15 16:13:59 +0300

    added data to mysql instance on start

--- Weight/set-env.sh
+++ Weight/set-env.sh
@@ -2,7 +2,10 @@
 # read in via: source <filename>
 export APP_DATA_PATH=/home/system/devops/w3/ganshmuelgreen/Weight/app/in
 export DB_DATA_PATH=/home/system/devops/w3/ganshmuelgreen/Weight/mysql/datadir
+# this script directory is intended for scripts to be manually run - during development
 export DB_SCRIPT_PATH=/home/system/devops/w3/ganshmuelgreen/Weight/mysql/scripts
+# this script directory is mounted into MySQL container in a location that causes it to auto-run all scripts there
+export DB_AUTO_RUN_SCRIPTS=/home/system/devops/w3/ganshmuelgreen/Weight/mysql/auto-run-scripts
 
 # set the environmental variables according to your filesystem, create directories if needed
 # run docker-compose up
