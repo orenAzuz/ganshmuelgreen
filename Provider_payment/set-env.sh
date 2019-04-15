a19614209596324fdeb18c72485d5bdb9bb8a867
nir <nirdod@gmail.com>
2019-04-15 11:21:52 +0300

    servers

---
+++ Provider_payment/set-env.sh
@@ -0,0 +1,7 @@
+# EXAMPLE
+# read in via: source <filename>
+export APP_DATA_PATH=/home/develeap/training/gans/Provider_payment
+export DB_DATA_PATH=/home/develeap/billdb
+export DB_SCRIPT_PATH=/home/develeap/training/gans/Provider_payment/sql
+export APP_EXT_PORT=8081
+export APP_INT_PORT=8000
