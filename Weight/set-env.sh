d0c96ec1bf394a34dfe5d71a2c35f5306e87f5aa
StackTracer <stacnospam@gmail.com>
2019-04-15 16:21:51 +0300

    fixed environment vars to be relative

--- Weight/set-env.sh
+++ Weight/set-env.sh
@@ -1,11 +1,11 @@
 # EXAMPLE
 # read in via: source <filename>
-export APP_DATA_PATH=/home/system/devops/w3/ganshmuelgreen/Weight/app/in
-export DB_DATA_PATH=/home/system/devops/w3/ganshmuelgreen/Weight/mysql/datadir
+export APP_DATA_PATH=./app/in
+export DB_DATA_PATH=./mysql/datadir
 # this script directory is intended for scripts to be manually run - during development
-export DB_SCRIPT_PATH=/home/system/devops/w3/ganshmuelgreen/Weight/mysql/scripts
+export DB_SCRIPT_PATH=./mysql/scripts
 # this script directory is mounted into MySQL container in a location that causes it to auto-run all scripts there
-export DB_AUTO_RUN_SCRIPTS=/home/system/devops/w3/ganshmuelgreen/Weight/mysql/auto-run-scripts
+export DB_AUTO_RUN_SCRIPTS=./mysql/auto-run-scripts
 
 # set the environmental variables according to your filesystem, create directories if needed
 # run docker-compose up
