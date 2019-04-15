cc6a8798be8441c13b5fbaef1953f2e9d0b5b29a
giulio <giuliovnturi@gmail.com>
2019-04-15 15:56:49 +0300

    improved stubs to serve billing app

--- Weight/app/app.py
+++ Weight/app/app.py
@@ -21,12 +21,27 @@ def unknown():
     return "OK"
 
 @app.route('/item/<id>')
-def item():
-    return "OK"
+def item(id):
+    result="""
+    { "id": 11,
+    "tara": 112,
+    "sessions": [1,2,3] 
+    }
+    """
+    return result
 
 @app.route('/session/<id>')
-def session():
-    return "OK"
+def session(id):
+    result="""
+    { "id": "mario", 
+    "truck": 11238,
+    "bruto": 9999,
+    "produce":"tomato",
+    "truckTara": 654,
+    "neto": 64564
+    }
+    """
+    return result
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=8000,debug=True)
