ad5df3f72856447cc5baa6b9a89bdcf505d7c356
nir <nirdod@gmail.com>
2019-04-15 14:45:40 +0300

    fix1

--- Provider_payment/app/app.py
+++ Provider_payment/app/app.py
@@ -1,22 +1,29 @@
 #!/usr/bin/env python
-import mysql.connector
+import flask
+from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route('/health')
 def health():
-    return "Payment team is OK"
+    if checkDBConnection() == 1:
+        return "Payment team is OK"
+    return "Failure"
 
 
-@app.route('/provider', methods=['POST'])
-def createProvider():
-    return "Create provider"
+@app.route('/provider/<name>', methods=['POST'])
+def createProvider(name):
+    query = "INSERT INTO Provider (`name`) VALUES ('" + name + "');"
+    runInsertQuery(query)
+    return "Create provider: " + name
 
 
-@app.route('/provider/<id>', methods=['PUT'])
-def updateProvider(id):
-    return "Update provider name by id:" + str(id)
+@app.route('/provider/<id>/<name>', methods=['PUT'])
+def updateProvider(id, name):
+    query = "update Provider set name=" + name + " where id=" + str(id) + ";"
+    runUpdateQuery(query)
+    return "Update provider name by id:" + str(id) + ". New name is: " + name
 
 
 @app.route('/rates', methods=['GET', 'POST'])
@@ -24,17 +31,21 @@ def rates():
     if flask.request.method == 'POST':
         return "Post rates"
     else:
-        return "Get rates"
+        return getRateFile()
 
 
-@app.route('/truck', methods=['POST'])
-def createTruck():
-    return "Create Truck"
+@app.route('/truck/<id>/<provider_id>', methods=['POST'])
+def createTruck(id, provider_id):
+    query = "INSERT INTO Trucks (`id`, `provider_id`) VALUES ('" + id + "', " + provider_id + ");"
+    runInsertQuery(query)
+    return "Create Truck: " + id + " for provider: " + provider_id
 
 
-@app.route('/truck/<id>', methods=['PUT'])
-def updateTruck(id):
-    return "Update Truck provider by license: " + str(id)
+@app.route('/truck/<id>/<provider_id>', methods=['PUT'])
+def updateTruck(id, provider_id):
+    query = "UPDATE Trucks SET provider_id='" + provider_id + "' WHERE id=" + id + ";"
+    runUpdateQuery(query)
+    return "Update Truck provider by license: " + str(id) + " new provider id is: " + provider_id
 
 
 @app.route('/truck/<id>', methods=['GET'])
@@ -49,6 +60,30 @@ def bill(id):
     t1 = flask.request.args.get("from")
     t2 = flask.request.args.get("to")
     return "Get Bill by id: " + str(id) + "and in range: " + str(t1) + ":" + str(t2)
+
+
+
+#local functions
+
+def checkDBConnection():
+    return 1
+    #run query
+
+def runUpdateQuery(query):
+    pass
+    #run query
+
+def runInsertQuery(query):
+    pass
+    #run query
+
+def getRateFile():
+    pass
+
+def createJsonResponse():
+    return ""
+    pass
+
 
 
 if __name__ == "__main__":
