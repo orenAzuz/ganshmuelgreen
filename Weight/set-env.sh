bd6ae3e6efc44d0052b28a2f7883ca79a139fa5b
JM <stacnospam@gmail.com>
2019-04-16 11:30:37 +0300

    works in container

--- Weight/set-env.sh
+++ Weight/set-env.sh
@@ -1,10 +1,14 @@
-# EXAMPLE
-# read in via: source <filename>
+# Read in via: source <filename>
+
 export APP_DATA_PATH=./app/in
+
+# NB: this needs to be "mysql-c" to run in the container (IT IS SET ACCORDINGLY IN docker-compose-yml).  For local DEV, this is localhost 
+export DB_HOST=localhost
+# This path is for DB data to persist regardless of container
 export DB_DATA_PATH=./mysql/datadir
-# this script directory is intended for scripts to be manually run - during development
+# This script directory is intended for scripts to be manually run - during development
 export DB_SCRIPT_PATH=./mysql/scripts
-# this script directory is mounted into MySQL container in a location that causes it to auto-run all scripts there
+# This script directory is mounted into MySQL container in a location that causes it to auto-run all scripts there
 export DB_AUTO_RUN_SCRIPTS=./mysql/auto-run-scripts
 
 # set the environmental variables according to your filesystem, create directories if needed
