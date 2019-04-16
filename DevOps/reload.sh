f92ea184baa4d02b7d75662f2d6218aed7db36f7
ilana <ilana.fisher.il@gmail.com>
2019-04-16 11:59:38 +0300

    fix up script

--- DevOps/reload.sh
+++ DevOps/reload.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 echo "reload in progress ...."
-cd $GIT_HOME
+cd $GIT_HOME/DevOps
+git checkout master
+git pull origin master
+
+echo $PWD
 . ./env.ini
 $SCRIPTS/down
 $SCRIPTS/up $(pwd) 
