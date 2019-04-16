ef3e49a8ab3480be7b7b978a0a090673feab69e3
nir <nirdod@gmail.com>
2019-04-16 11:10:51 +0300

    fix ports

--- Provider_payment/set-env.sh
+++ Provider_payment/set-env.sh
@@ -4,6 +4,6 @@ export APP_DATA_PATH=$PWD
 export DB_DATA_PATH=$HOME/billdb
 export DB_SCRIPT_PATH=$PWD/sql
 export APP_EXT_PORT=8081
-export APP_INT_PORT=8000
+export APP_INT_PORT=8080
 export DB_EXT_PORT=8082
 export DB_INT_PORT=3306
