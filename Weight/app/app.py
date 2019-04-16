bf3c8d5e3b218ba1ca248718e0d01213277ac50a
raz <razleshem3@gmail.com>
2019-04-16 17:11:06 +0300

    Weight get

--- Weight/app/app.py
+++ Weight/app/app.py
@@ -1,15 +1,38 @@
 #!/usr/bin/env python
-from flask import Flask
-import mysql.connector
+from flask import Flask,request
+import mysql.connector, time
+import json
 app = Flask(__name__)
 
 @app.route('/health')
 def health():
     return "OK"
 
-@app.route('/weight')
+@app.route('/weight',methods = ['GET'])
 def weight():
-    return "OK"
+    mydb = mysql.connector.connect(
+      host="mysql-db",
+      user="root",
+      passwd="greengo",
+      database="weight",
+      auth_plugin='mysql_native_password'
+    )
+    tim = time.strftime("%Y%m%d%H%M%S", time.gmtime())
+    timt = time.strftime("%Y%m%d000000", time.gmtime())
+    t1 = request.args.get('from', default = timt)
+    t2 = request.args.get('to', default = tim)
+    f = request.args.get('filter', default = "'in','out','none'")
+    mycursor = mydb.cursor()
+    arg = mycursor.execute("SELECT id,direction,bruto,neto,produce,containers FROM transactions WHERE direction IN (" + f + ") AND datetime BETWEEN "+t1+" AND " +t2)
+    result = mycursor.fetchall()
+    json_data = []
+    row_headers = [val[0] for val in mycursor.description]
+    #json_data.append(dict(zip(row_headers, result)))
+    for x in result:
+        content = {'id': x[0], 'direction': x[1], 'bruto': x[2], 'neto': x[3], 'produce': x[4], 'containers':x[5]}
+        json_data.append(content)
+        content = {}
+    return json.dumps(json_data)
 
 @app.route('/batch-weight')
 def batch_weight():
