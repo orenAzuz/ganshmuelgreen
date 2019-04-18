92955376254e7e34b8fcd15d99e4301e188d1696
dakar <dakar1412@gmail.com>
2019-04-18 11:43:14 +0300

    finishing bill API

--- Provider_payment/app/app.py
+++ Provider_payment/app/app.py
@@ -12,8 +12,10 @@ import csv
 app = Flask(__name__)
 
 # Consts
-HOST = 'mysql-db'
-#HOST = '0.0.0.0'
+#DBHOST = 'mysql-db'
+DBHOST = '0.0.0.0'
+#HOST = '18.222.236.224'
+HOST='0.0.0.0'
 
 
 @app.route('/health')
@@ -77,10 +79,10 @@ def getTruck(id):
     t1 = flask.request.args.get("from")
     t2 = flask.request.args.get("to")
     try:
-        weightUrl = "http://18.222.236.224/item/%s?from=%s&to=%s" % (str(id), str(t1), str(t2))
+        weightUrl = "http://%s:8081/item/%s?from=%s&to=%s" % (HOST, str(id), str(t1), str(t2))
         response = urllib.request.urlopen(weightUrl)
         return response.read()
-    except (urllib.HTTPError, urllib.URLError, http.client.HTTPException, Exception):
+    except (http.client.HTTPException, Exception):
         return '{"id":1234, "tara":85000, "sessions":[10]}'
 
 
@@ -103,71 +105,76 @@ def bill(providerId):
             query = "SELECT name FROM Provider WHERE id=%s;"
             cursor.execute(query, (str(providerId)), )
             result = cursor.fetchone()
-            providerName = result['name']
+            if result is not None:
+                providerName = result['name']
 
-            # Get provider trucks
-            query = "SELECT id FROM Trucks WHERE provider_id=%s;"
-            cursor.execute(query, (str(providerId)), )
-            truckRows = cursor.fetchall()
-            for truckRow in truckRows:
-                truckCount += 1  # count trucks
-                truck = truckRow['id']
-                # truckStr = getTruck(truck) TODO from weight team
-                truckStr = '{"id":10000, "tara":1200, "sessions":[11]}'  # Get truck data from API
-                truckData = json.loads(truckStr)  # Convert to json object
-                sessions = truckData['sessions']
-                sessionCount += len(sessions)  # accumulate sessions
-                for sessionId in sessions:
-                    # sessionUrl = "http://18.222.236.224:8081/session/%s" % str(id) TODO change to production
-                    sessionUrl = "http://0.0.0.0:8081/session/%s" % str(id)
-                    sessionStr = urllib.request.urlopen(sessionUrl)
-                    #sessionStr = '{"id":10, "truck":"134-33-443", "bruto":70000, "truckTara":85000, "produce":"Blood", "neto":60000}'  # Get session data
-                    sessionData = json.loads(sessionStr)  # Convert to json object
+                # Get provider trucks
+                query = "SELECT id FROM Trucks WHERE provider_id=%s;"
+                cursor.execute(query, (str(providerId)), )
+                truckRows = cursor.fetchall()
+                for truckRow in truckRows:
+                    truckCount += 1  # count trucks
+                    truck = truckRow['id']
+                    truckStr = getTruck(truck)
+                    #truckStr = '{"id":10000, "tara":1200, "sessions":[11]}'  # Get truck data from API
+                    truckData = json.loads(truckStr)  # Convert to json object
+                    print(truckData)
+                    sessions = truckData['sessions']
+                    sessionCount += len(sessions)  # accumulate sessions
+                    for sessionId in sessions:
+                        sessionUrl = "http://%s:8081/session/%s" % (HOST, str(sessionId))
+                        response = urllib.request.urlopen(sessionUrl)
+                        sessionStr = response.read()
+                        sessionData = json.loads(sessionStr)  # Convert to json object
 
-                    # Initialize produce in products table
-                    if products.get(sessionData['produce']) is None:
-                        products[sessionData['produce']] = {}
+                        # Initialize produce in products table
+                        if products.get(sessionData['produce']) is None:
+                            products[sessionData['produce']] = {}
 
-                    # Set product name
-                    products[sessionData['produce']]['produce'] = sessionData['produce']
+                        # Set product name
+                        products[sessionData['produce']]['produce'] = sessionData['produce']
 
-                    # Set accumulation of product sessions count
-                    if products[sessionData['produce']].get('count'):
-                        products[sessionData['produce']]['count'] += 1
-                    else:
-                        products[sessionData['produce']]['count'] = 1
-
-                    # Set accumulation of product neto tara amount
-                    if products[sessionData['produce']].get('amount'):
-                        products[sessionData['produce']]['amount'] += sessionData['neto']
-                    else:
-                        products[sessionData['produce']]['amount'] = sessionData['neto']
-
-                    # Get provider rate
-                    query = "select rate,scope from Rates where scope in ('All',%s) and product_id=%s;"
-                    cursor.execute(query, (str(providerId), sessionData['produce']), )
-                    rateRows = cursor.fetchall()
-                    rate, rateAll = 0, 0
-                    for rateRow in rateRows:
-                        if [[rateRow['scope'] == str(providerId)]]:
-                            rate = rateRow['rate']
-                            print(rate)
-                            break;
+                        # Set accumulation of product sessions count
+                        if products[sessionData['produce']].get('count'):
+                            products[sessionData['produce']]['count'] += 1
                         else:
-                            rateAll = rateRow['rate']
-                    if not rate:
-                        rate = rateAll
-                    products[sessionData['produce']]['rate'] = rate
+                            products[sessionData['produce']]['count'] = 1
 
-                    # Calc pay from rate and amount and accumulate product pay
-                    if products[sessionData['produce']].get('pay'):
-                        products[sessionData['produce']]['pay'] += products[sessionData['produce']]['amount'] * \
-                                                                   products[sessionData['produce']]['rate']
-                    else:
-                        products[sessionData['produce']]['pay'] = products[sessionData['produce']]['amount'] * \
-                                                                  products[sessionData['produce']]['rate']
+                        # Set accumulation of product neto tara amount
+                        if sessionData.get('neto'):
+                            if products[sessionData['produce']].get('amount'):
+                                products[sessionData['produce']]['amount'] += sessionData['neto']
+                            else:
+                                products[sessionData['produce']]['amount'] = sessionData['neto']
 
-                    totalPay += products[sessionData['produce']]['pay']  # Accumulate all pays for all products
+                            # Get provider rate
+                            query = "select rate,scope from Rates where scope in ('All',%s) and product_id=%s;"
+                            cursor.execute(query, (str(providerId), sessionData['produce']), )
+                            rateRows = cursor.fetchall()
+                            rate, rateAll = 0, 0
+                            for rateRow in rateRows:
+                                if [[rateRow['scope'] == str(providerId)]]:
+                                    rate = rateRow['rate']
+                                    break;
+                                else:
+                                    rateAll = rateRow['rate']
+                            if not rate:
+                                rate = rateAll
+                            products[sessionData['produce']]['rate'] = rate
+
+                            # Calc pay from rate and amount and accumulate product pay
+                            if products[sessionData['produce']].get('pay'):
+                                products[sessionData['produce']]['pay'] += products[sessionData['produce']]['amount'] * \
+                                                                           products[sessionData['produce']]['rate']
+                            else:
+                                products[sessionData['produce']]['pay'] = products[sessionData['produce']]['amount'] * \
+                                                                          products[sessionData['produce']]['rate']
+
+                            totalPay += products[sessionData['produce']]['pay']  # Accumulate all pays for all products
+            else:
+                return "Provider hasn't been found"
+                connection.commit()
+                connection.close()
 
         connection.commit()
     finally:
@@ -206,7 +213,7 @@ def checkDBConnection():
 
 
 def getConnection():
-    return pymysql.connect(host=HOST, port=3306, user="root", passwd="greengo", db="billdb", charset='utf8mb4',
+    return pymysql.connect(host=DBHOST, port=3306, user="root", passwd="greengo", db="billdb", charset='utf8mb4',
                            cursorclass=pymysql.cursors.DictCursor)
 
 
@@ -263,4 +270,4 @@ def createJsonResponse():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    app.run(host="0.0.0.0", port=8085, debug=True)
