c3619e1c9fa505c81717075d96b28e5846ef46e0
JM <stacnospam@gmail.com>
2019-04-18 09:39:17 +0300

    tuning for prod-run test scripts

--- Weight/app/test-data/live/run-test-batch-weight.sh
+++ Weight/app/test-data/live/run-test-batch-weight.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo BATCH-WEIGHT ENDPOINT:
+echo ======================
+echo
+
 echo Read from containers1.csv \(in kg\), write to database and show output - if data are not already in:
 ./test-batch-weight.sh 8081 containers1.csv
 echo
